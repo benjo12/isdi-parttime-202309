6e27c8d16c43b0d5501b5521312c128fe62d5781
benjo12 <cmayiba@hotmail.com>
2023-09-22 21:46:36 +0200

    add last  updates

--- staff/benjamin-mayiba/frontend/app/main.js
+++ staff/benjamin-mayiba/frontend/app/main.js
@@ -1,1 +1,123 @@
-document.getElementById("register").style.display = "none"
+// storage
+
+var users = []
+
+// Register
+
+var registerView = document.getElementById('register')
+
+registerView.style.display = 'none'
+
+var registerLoginLink = registerView.querySelector('a')
+
+registerLoginLink.onclick = function(event){
+    event.preventDefault()
+ 
+  registerView.style.display = 'none'
+  loginView.style.display = 'block'
+ 
+}
+
+var registerForm = registerView.querySelector('form')
+
+registerForm.onsubmit = function(event){
+    event.preventDefault()
+ 
+  var nameInput = registerForm.querySelector('#name')
+    var emailInput = registerForm.querySelector('#email')
+    var passwordInput = registerForm.querySelector('#password')
+
+    var name = nameInput.value
+    var email = emailInput.value
+    var password = passwordInput.value
+   
+    var user = {}
+   
+    user.name = name
+    user.email = email
+    user.password = password  
+ 
+  // TO DO check user is new, otherwise show error
+ 
+   var userFound = users.find(function(userCheked){
+     return(
+        userCheked.name === user.name &&
+       userCheked.email === user.email &&
+       userCheked.password === user.password
+     )
+   })
+     if(userFound){
+       console.log('This user already exists')
+     }else{
+       users.push(user)
+     }
+     
+ 
+     nameInput.value = ''
+     emailInput.value = ''
+     passwordInput.value = ''
+}
+
+//login
+
+var loginView = document.getElementById('login')
+
+var loginRegisterLink = loginView.querySelector('a')
+
+loginRegisterLink.onclick = function(event){
+     event.preventDefault()
+ 
+    loginView.style.display = 'none'
+    registerView.style.display = 'block'
+}
+
+// TODO implement login functionality
+// start home
+var homeView = document.getElementById('home')
+
+var h1View = homeView.querySelector('h1')
+
+ homeView.style.display = 'none'
+// end home
+
+var registerLoginForm = loginView.querySelector('form')
+
+registerLoginForm.onsubmit = function(event){
+     event.preventDefault()
+  var nameToDisplay = ''
+  var emailLoginInput = registerLoginForm.querySelector('#email')
+  var passwordLoginInput = registerLoginForm.querySelector('#password')
+ 
+  var emailLogin = emailLoginInput.value
+  var passwordLogin = passwordLoginInput.value
+   
+  var userLogged = users.find(function(user){
+    return (user.email === emailLogin && user.password === passwordLogin);
+})
+   
+   if(userLogged){
+    nameToDisplay = userLogged.name
+     console.log('Hello '+ nameToDisplay)
+     h1View.textContent = 'Hello, ' + nameToDisplay; // Update the h1View content
+
+    homeView.style.display = 'block';
+   }else{
+     console.log('User is not registered. Please register the user first.')
+   }
+ 
+   emailLoginInput.value = ''
+   passwordLoginInput.value = ''
+   
+}
+
+
+// home
+/*
+var homeView = document.getElementById('home')
+
+var h1View = homeView.querySelector('h1')
+var h1Content = h1View.textContent
+   
+homeView.style.display = 'none' */
+
+// TODO show user name logged in when entering in Home (Hello, >name<!)
