ddd34b9e29376807e8a1a856886c0e5434a43e40
benjo12 <cmayiba@hotmail.com>
2023-10-20 21:55:53 +0200

    update arrays indexOf function

--- staff/benjamin-mayiba/frontend/arrays/indexOf.js
+++ staff/benjamin-mayiba/frontend/arrays/indexOf.js
@@ -1,4 +1,4 @@
-function concat(arr,element){
+function indexOf(arr,element){
     for(var i = 0; i<arr.length; i++){
        if(arr[i] === element) return i
     }
--- staff/benjamin-mayiba/frontend/arrays/indexOf.spec.js
+++ staff/benjamin-mayiba/frontend/arrays/indexOf.spec.js
@@ -1,20 +1,20 @@
 console.log('TEST indexOf for Array')
 console.log('CASE for ["potato", "tomato", "chillies", "green-pepper"] indexOf "tomato" results in 1')
-console.log(concat(["potato", "tomato", "chillies", "green-pepper"], 'tomato'))
+console.log(indexOf(["potato", "tomato", "chillies", "green-pepper"], 'tomato'))
 // 1
 
 console.log('CASE for ["potato", "tomato", "chillies", "green-pepper"] indexOf "camel" results in -1')
-console.log(concat(["potato", "tomato", "chillies", "green-pepper"], 'camel'))
+console.log(indexOf(["potato", "tomato", "chillies", "green-pepper"], 'camel'))
 // -1
 
 console.log('CASE for [2, 9, 9] indexOf 2 results in 0')
-console.log(concat([2, 9, 9], 2))
+console.log(indexOf([2, 9, 9], 2))
 // 0
 
 console.log('CASE for [2, 9, 9] indexOf 9 results in 1')
-console.log(concat([2, 9, 9], 9))
+console.log(indexOf([2, 9, 9], 9))
 // 1
 
 console.log('CASE for [2, 9, 9] indexOf 14 results in -1')
-console.log(concat([2, 9, 9], 14))
+console.log(indexOf([2, 9, 9], 14))
 // -1
