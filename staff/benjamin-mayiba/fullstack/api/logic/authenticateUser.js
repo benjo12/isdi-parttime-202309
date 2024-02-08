8412d5a96375db5b1a129a6041f443a900beae35
Benjamin Mayiba <cmayiba@hotmail.com>
2024-02-08 16:20:08 +0100

    update last commit, add authenticate user #209

--- staff/benjamin-mayiba/fullstack/api/logic/authenticateUser.js
+++ staff/benjamin-mayiba/fullstack/api/logic/authenticateUser.js
@@ -10,23 +10,31 @@ function authenticateUser(email, password) {
     validate.email(email, 'email')
     validate.password(password, 'password')
 
-    return User.findOne({ email })
-        .catch(error => {
-            throw new SystemError(error.message)
-        })
-        .then(user => {
-            if (!user)
-                throw new NotFoundError('user not found')
+    return (async () => {
+        let user
 
-            return bcrypt.compare(password, user.password)
-                .catch(error => { throw new SystemError(error.message) })
-                .then(match => {
-                    if (!match)
-                        
-                        throw new CredentialsError('wrong password')
-                    return user.id
-                })
-        })
+        try {
+            user = await User.findOne({ email })
+        } catch (error) {
+            throw new SystemError(error.message)
+        }
+
+        if (!user)
+            throw new NotFoundError('user not found')
+
+        let match
+
+        try {
+            match = await bcrypt.compare(password, user.password)
+        } catch (error) {
+            throw new SystemError(error.message)
+        }
+
+        if (!match)
+            throw new CredentialsError('wrong password')
+
+        return user.id
+    })()
 }
 
 export default authenticateUser
